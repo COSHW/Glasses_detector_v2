68617e521dd6cf27f48edb101401be2540fc2c58
Roman <45237915+COSHW@users.noreply.github.com>
2019-10-02 13:37:31 +0300

    Bug fix

--- get_model_accuracy.py
+++ get_model_accuracy.py
@@ -7,7 +7,7 @@ X = pickle.load(pickle_in)
 
 pickle_in = open(r"saved_data\y.pickle", "rb")
 y = pickle.load(pickle_in)
-X = X.reshape(-1, 200, 200, 1)
+X = X.reshape(-1, 100, 100, 1)
 X = X/255.0
 
 model = load_model(r"saved_data\model.h5")
